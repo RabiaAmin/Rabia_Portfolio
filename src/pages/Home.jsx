e89725c052d00027a089e8ce13e91a70686289ab
RabiaAmin <rabia10march@gmail.com>
2025-05-18 21:46:40 +0200

    animated background added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import ThemeToggle from '../component/ThemeToggle'
+import Threads from '../component/ThreadsBackground'
 
 function Home() {
   return (
-    <div className='min-h-screen '>
+    <div className='min-h-screen bg-background text-foreground overflow-x-hidden'>
     {/* Theme Toggle */}
+    <ThemeToggle />
     {/* Background Effeect */}
+    <div style={{ width: '100vw', height: '100vh', position: 'relative'  }}>
+  <Threads
+    amplitude={3}
+    distance={1.1}
+    enableMouseInteraction={false}
+  />
+</div>
     {/* Navbar */}
     {/* Main Content*/}
     {/* Footer*/}
